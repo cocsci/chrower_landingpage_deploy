566cc9cf7e86b37321500931ffe78e9af1c58542
haukdaniel <daniel.hauk@codecamp-n.com>
2020-10-07 13:33:53 +0200

    fontFamily + fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
       backgroundImage: (theme) => ({
         "hero-pattern": "url('/images/hero_bg.png')",
       }),
+      fontFamily: {
+        'headline': ['Amiko'],
+      },
+      fontSize: {
+        'headline': '48px',
+        'sub-headline': '28px',
+        'article-headline': '20px',
+        'article': '16px'
+      }
     },
   },
   variants: {},
