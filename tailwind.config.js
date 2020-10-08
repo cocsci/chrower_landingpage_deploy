d25cf84942eef3fcf7e862ff437c45ae0c2cd69f
haukdaniel <daniel.hauk@codecamp-n.com>
2020-10-08 11:49:24 +0200

    favicon, new text, hyphens

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       }),
       fontFamily: {
         headline: ["Amiko"],
+        normal: ["BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
       },
       fontSize: {
         headline: "48px",
